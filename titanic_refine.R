4b12df5a3af3193e784ed710c334026c4263eeb6
Ryan Transfiguracion <ryanthtra@MacBook-Pro.frontierlocal.net>
2018-03-13 11:57:15 -0700

    Removed call to View()

--- titanic_refine.R
+++ titanic_refine.R
@@ -3,7 +3,6 @@ library(readr)
 
 # Step 0 - Load the data in RStudio
 titanic_original <- read_csv("titanic_original.csv")
-View(titanic_original)
 
 # Step 1 - Port of embarkation
 titanic_clean <- titanic_original %>% 
