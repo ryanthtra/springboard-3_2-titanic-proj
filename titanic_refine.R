7348e97fc2d215b4f909153ec29dbf2016596b76
Ryan Transfiguracion <ryanthtra@MacBook-Pro.frontierlocal.net>
2018-03-13 00:22:53 -0700

    Step 1 complete.  Typo fix in step 0, as well.

--- titanic_refine.R
+++ titanic_refine.R
@@ -2,7 +2,9 @@ library(dplyr)
 library(readr)
 
 # Step 0 - Load the data in RStudio
-refine_original <- read_csv("titanic_original.csv")
-View(refine_original)
+titanic_original <- read_csv("titanic_original.csv")
+View(titanic_original)
 
 # Step 1 - Port of embarkation
+titanic_clean <- titanic_original %>% 
+  mutate(embarked = replace(embarked, is.na(embarked) == TRUE, 'S'))
