047a97d31464bb1cc068adc9fc8984ff11bee40d
Ryan Transfiguracion <ryanthtra@MacBook-Pro.frontierlocal.net>
2018-03-13 11:38:43 -0700

    Step 5 completed

--- titanic_refine.R
+++ titanic_refine.R
@@ -13,13 +13,19 @@ titanic_clean <- titanic_original %>%
 # 1. Calculate the mean of the Age column, and use that value to populate the missing values.
 mean_age <- titanic_original %>% 
   summarise(avg = mean(age, na.rm = TRUE))
+# Convert mean_age to a single numeric value
+mean_age <- as.numeric(unlist(mean_age[1,1])) 
 titanic_clean <- titanic_clean %>% 
-  mutate(age = replace(age, is.na(age) == TRUE, round(mean_age[1,1], 2)))
+  mutate(age = replace(age, is.na(age) == TRUE, round(mean_age, 2)))
 
 # Step 3 - Lifeboat: replace missing boat values with 'None'
 titanic_clean <- titanic_clean %>% 
   mutate(boat = replace(boat, is.na(boat) == TRUE, 'None'))
 
 # Step 4 - Cabin
+# Some of the passengers didn't have a cabin, like the poorer people -- at least the movie indicated it so ;-)
 titanic_clean <- titanic_clean %>% 
   mutate(has_cabin_number = if_else(is.na(cabin), 0, 1))
+
+# Step 5 - Submit the project on Github (create clean .csv file)
+write_csv(titanic_clean, "./titanic_clean.csv")
